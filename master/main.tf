f13e91d2aa640e7c3f0adedbb878a3108ee373ae
Subham Jena <subhamjena68@gmail.com>
2023-06-16 12:18:45 +0000

    more modifications

--- master/main.tf
+++ master/main.tf
@@ -1,10 +1,10 @@
 module "master_node" {
 	source = "../module1"
-	ami             = ""
-	instance_type   = ""
-	key_name        = ""
-	subnet       = ""
-	security_group = ""
-	path = ""
+	ami             = ""   # image id
+	instance_type   = ""   # instance type ex: t2.micro
+	key_name        = ""  # keyname ex: key
+	subnet       = ""  # subnet id 
+	security_group = ""   # security group id
+	path = ""   #absolute path of .pem file ex  /home/username/key.pem
 
 }
